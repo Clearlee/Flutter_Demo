964cdfa7b1c6cd8f824bb181107ec78ec907890e
Clearleee <13094446038@163.com>
2019-07-12 17:31:20 +0800

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/page/animate_weight.dart';
 import 'package:flutter_app/page/container_weight.dart';
-import 'package:flutter_app/page/drag_weight.dart';
-import 'package:flutter_app/page/home_weight.dart';
-import 'package:flutter_app/page/login_weight.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,12 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: ContainerPage(),
-//      routes: {
-//        'login_page': (context) => LoginPage(),
-//        'home_page': (context) => HomeWeight(),
-//        'drag_page': (context) => DragWeight(),
-//        'animate_page': (context) => AnimateWeight(),
-//      },
     );
   }
 }
